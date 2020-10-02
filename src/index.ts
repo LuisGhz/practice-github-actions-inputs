a46aa46d45a90df68e166a2906a1b6278b4aa3d6
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-02 10:29:34 -0500

    Add console.log

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core';
 function run() {
   const name: string = core.getInput('my_input');
   if (name) {
+    console.log(name);
     core.debug(`Hello ${name}!`);
     return core.setOutput('my_output', `Hello ${name}!`);
   }
