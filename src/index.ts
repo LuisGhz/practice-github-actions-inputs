f411c0991b9fe31f3d1344aef24399396e39c55c
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-02 14:39:18 -0500

    Install yaml & parse objectStr

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import * as core from '@actions/core';
+const YAML = require('yaml');
 
 function run() {
   const name: string = core.getInput('my_input');
+  const objectStr: any = core.getInput('my_object');
+
   if (name) {
     console.log(name);
-    core.debug(`Hello ${name}!`);
+    core.info(`Hello ${name}!`);
     return core.setOutput('my_output', `Hello ${name}!`);
+  } else {
+    core.setFailed('my_input not specified!');
   }
-  core.setFailed('my_input not specified!');
+
+  if(objectStr) {
+    const object = YAML.parse(objectStr)
+    console.log(object)
+  }
 }
 
 run();
